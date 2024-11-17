b93acc52b547932b8680eee28f10d5e2aa38e719
m-oooooonchild <syp614@ewhain.net>
2024-11-17 19:04:17 +0900

    🐛 타입스크립트 설치

--- week06/app/(movies)/movies/[id]/page.tsx
+++ week06/app/(movies)/movies/[id]/page.tsx
@@ -2,18 +2,20 @@ import { Suspense } from 'react';
 import MovieInfo, { getMovie } from '../../../../components/movie-info';
 import MovieVideos from '../../../../components/movie-videos';
 
-interface IParams {
-  params: { id: string };
-}
-
-export async function generateMetadata({ params: { id } }: IParams) {
-  const movie = await getMovie(id);
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const movie = await getMovie(params.id);
   return {
     title: movie.title,
   };
 }
 
-export default async function MovieDetailPage({ params: { id } }: IParams) {
+export default async function MovieDetailPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { id } = params;
+
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
@@ -26,4 +28,5 @@ export default async function MovieDetailPage({ params: { id } }: IParams) {
   );
 }
 
+// 런타임 환경 설정
 export const runtime = 'edge';
